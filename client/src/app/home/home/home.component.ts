b1c017534ea5e9258da1656d21e0bf72bfc202d8
Binh Hoang <hbinhit221@gmail.com>
2022-05-01 21:58:09 +0700

    Change home ui

--- client/src/app/home/home/home.component.ts
+++ client/src/app/home/home/home.component.ts
@@ -9,10 +9,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   userName:string = '';
   constructor(private router: Router) { }
+  enterpriseCount = 0;
+  userCount = 0;
+  percentCount = 0;
 
   ngOnInit(): void {
   }
-
+  enterpriseCountStop = setInterval(()=>{
+    this.enterpriseCount ++;
+    if(this.enterpriseCount  === 25){
+      clearInterval(this.enterpriseCountStop)
+    }
+  }, 50)
+  userCountStop = setInterval(()=>{
+    this.userCount ++;
+    if(this.userCount  === 250){
+      clearInterval(this.userCountStop)
+    }
+  }, 1)  
+  percentCountStop = setInterval(()=>{
+    this.percentCount ++;
+    if(this.percentCount  === 92){
+      clearInterval(this.percentCountStop)
+    }
+  }, 20)  
   navigateToTask(){
     this.router.navigateByUrl('projects')
   }
