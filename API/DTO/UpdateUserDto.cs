f339fcb659df5666ef2ac8cc441ca168912446b7
An <an0994305@gmail.com>
2022-05-12 00:01:22 +0700

    API-Update-Acc

---
+++ API/DTO/UpdateUserDto.cs
@@ -0,0 +1,17 @@
+using API.Enum;
+using System;
+
+namespace API.DTO
+{
+    public class UpdateUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public Guid DepartmentId { get; set; }
+        public Permission PermissionCode { get; set; }
+    }
+}
