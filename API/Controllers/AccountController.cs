0b69422cfc591c60bae051e2c0a0d0d5034947dc
ADMIN\Admin <bykmaimai@gmail.com>
2022-05-18 21:05:48 +0700

    API-Register-with-Permission

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -79,28 +79,60 @@ namespace API.Controllers
             if (newUser != null) return BadRequest("Username is taken");
             var leaderExisted = await _dataContext.AppUser.AsNoTracking().FirstOrDefaultAsync(e => e.DepartmentId == input.DepartmentId && e.PermissionCode == Permission.Leader);
             if (leaderExisted != null && input.PermissionCode == Permission.Leader) return BadRequest("Department had leader");
-            var user = new AppUser
+            if (input.PermissionCreator == Permission.Leader && input.PermissionCode == Permission.Employee)
             {
-                Id = new Guid(),
-                FirstName = input.FirstName,
-                LastName = input.LastName,
-                Address = input.Address,
-                Email = input.Email,
-                Phone = input.Phone,
-                Password = input.Password,
-                DepartmentId = input.DepartmentId,
-                PermissionCode = input.PermissionCode
-            };
-            await _dataContext.AppUser.AddAsync(user);
-            await _dataContext.SaveChangesAsync();
-            return new AppUserDto
+                var user = new AppUser
+                {
+                    Id = new Guid(),
+                    FirstName = input.FirstName,
+                    LastName = input.LastName,
+                    Address = input.Address,
+                    Email = input.Email,
+                    Phone = input.Phone,
+                    Password = input.Password,
+                    DepartmentId = input.DepartmentId,
+                    PermissionCode = input.PermissionCode
+                };
+                await _dataContext.AppUser.AddAsync(user);
+                await _dataContext.SaveChangesAsync();
+                return new AppUserDto
+                {
+                    Id = user.Id,
+                    PermissionCode = user.PermissionCode,
+                    Email = user.Email,
+                    DepartmentId = user.DepartmentId,
+                    Token = _tokenService.CreateToken(user)
+                };
+            }
+            else if (input.PermissionCreator == Permission.ProjectManager && (input.PermissionCode == Permission.Employee || input.PermissionCode == Permission.Leader))
             {
-                Id = user.Id,
-                PermissionCode = user.PermissionCode,
-                Email = user.Email,
-                DepartmentId = user.DepartmentId,
-                Token = _tokenService.CreateToken(user)
-            };
+                var user = new AppUser
+                {
+                    Id = new Guid(),
+                    FirstName = input.FirstName,
+                    LastName = input.LastName,
+                    Address = input.Address,
+                    Email = input.Email,
+                    Phone = input.Phone,
+                    Password = input.Password,
+                    DepartmentId = input.DepartmentId,
+                    PermissionCode = input.PermissionCode
+                };
+                await _dataContext.AppUser.AddAsync(user);
+                await _dataContext.SaveChangesAsync();
+                return new AppUserDto
+                {
+                    Id = user.Id,
+                    PermissionCode = user.PermissionCode,
+                    Email = user.Email,
+                    DepartmentId = user.DepartmentId,
+                    Token = _tokenService.CreateToken(user)
+                };
+            }
+            else
+            {
+                return BadRequest("You must had permission");
+            }
         }
 
         [HttpPost("login")]
