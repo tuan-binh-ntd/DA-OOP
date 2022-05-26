104ea041559e8b88aec0fc9506829bb7ae3a27ea
Binh Hoang <hbinhit221@gmail.com>
2022-05-27 00:40:28 +0700

    - Sua layout home

--- client/src/app/routes/home/home.component.ts
+++ client/src/app/routes/home/home.component.ts
@@ -2,36 +2,65 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { catchError, of } from 'rxjs';
+import { ProjectService } from 'src/app/services/project.service';
+import {StatusCode} from 'src/app/helpers/StatusCodeEnum';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-   pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ],
-    datasets: [ {
-      data: [ 300, 500, 100 ]
-    } ]
-  };
+  
+  projects: any[] = [];
+  reopenedProject: number = 0;
+  openProject: number = 0;
+  completeProject: number = 0;
+  inProgressProject: number = 0;
+   pieChartData: ChartData<'pie', number[], string | string[]> 
    pieChartType: ChartType = 'pie';
    pieChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     plugins: {
       legend: {
-        display: true,
-        position: 'top',
+        display: false,
       },
     }
   };
   enterpriseCount = 0;
   userCount = 0;
   percentCount = 0;
- constructor(private router: Router){}
- ngOnInit(): void {
-
+ constructor(private router: Router, private projectService: ProjectService){}
+async ngOnInit() {
+ await this.fetchProjectData();
+  this.divideProjectData();
  }
+
+async fetchProjectData() {
+ await this.projectService
+    .getAllProject()
+    .toPromise().then((response) => {
+      this.projects = response;
+    });
+}
+
+  divideProjectData(){
+    this.reopenedProject = this.projects.filter(project=> project.statusCode === StatusCode.Reopened).length;
+    this.openProject = this.projects.filter(project=> project.statusCode === StatusCode.Open).length;
+    this.inProgressProject = this.projects.filter(project=> project.statusCode === StatusCode.InProgress).length;
+    this.completeProject = this.projects.filter(project=> project.statusCode === StatusCode.Resolved).length;
+  
+    this.pieChartData = {
+      labels: ['Reopened Projects', 'Open Projects', 'Completed Projects', 'Inprogress Projects' ],
+      datasets: [ {
+        data: [ this.reopenedProject, this.openProject, this.completeProject, this.inProgressProject],
+      } ]
+    };
+    const image = new Image();
+image.src = 'assets/home-header-bg.jpg';
+  }
 
  enterpriseCountStop = setInterval(()=>{
   this.enterpriseCount ++;
--- client/src/app/services/project.service.ts
+++ client/src/app/services/project.service.ts
@@ -9,21 +9,25 @@ export class ProjectService {
   baseUrl = "https://localhost:5001/api/project";
   constructor(private http: HttpClient) {
   }
-  getAllProject(payload: any):Observable<any>{
-    let keyword,status,type, priority, permision,createDateFrom,createDateTo,deadlineDateFrom,deadlineDateTo,departmentId,userId
-        keyword = payload.keyWord ? 'keyWord=' + payload.keyWord  : ''
-         status = payload.statusCode ? '&statusCode=' + payload.statusCode  : ''
-        type = payload.projectType ? '&projectType=' + payload.projectType  : ''
-        priority = payload.priorityCode ? '&priorityCode=' + payload.priorityCode  : ''
-        permision = payload.permision ? '&permision=' + payload.permision  : ''
-        createDateFrom = payload.createDateFrom ? '&createDateFrom=' + payload.createDateFrom  : ''
-        createDateTo = payload.createDateTo ? '&createDateTo=' + payload.createDateTo  : ''
-        deadlineDateFrom = payload.deadlineDateFrom ? '&deadlineDateFrom=' + payload.deadlineDateFrom  : ''
-        deadlineDateTo = payload.deadlineDateTo ? '&deadlineDateTo=' + payload.deadlineDateTo  : ''
-        departmentId = payload.departmentId ? '&departmentId=' + payload.departmentId  : ''
-        userId = payload.userId ? '&userId=' + payload.userId  : ''
-      return this.http.get(this.baseUrl + '/getall?' + keyword + type + status + priority
-      + permision + createDateFrom + createDateTo + deadlineDateFrom + deadlineDateTo + departmentId + userId);
+  getAllProject(payload?: any):Observable<any>{
+    if(payload){
+      let keyword,status,type, priority, permision,createDateFrom,createDateTo,deadlineDateFrom,deadlineDateTo,departmentId,userId
+          keyword = payload.keyWord ? 'keyWord=' + payload.keyWord  : ''
+           status = payload.statusCode ? '&statusCode=' + payload.statusCode  : ''
+          type = payload.projectType ? '&projectType=' + payload.projectType  : ''
+          priority = payload.priorityCode ? '&priorityCode=' + payload.priorityCode  : ''
+          permision = payload.permision ? '&permision=' + payload.permision  : ''
+          createDateFrom = payload.createDateFrom ? '&createDateFrom=' + payload.createDateFrom  : ''
+          createDateTo = payload.createDateTo ? '&createDateTo=' + payload.createDateTo  : ''
+          deadlineDateFrom = payload.deadlineDateFrom ? '&deadlineDateFrom=' + payload.deadlineDateFrom  : ''
+          deadlineDateTo = payload.deadlineDateTo ? '&deadlineDateTo=' + payload.deadlineDateTo  : ''
+          departmentId = payload.departmentId ? '&departmentId=' + payload.departmentId  : ''
+          userId = payload.userId ? '&userId=' + payload.userId  : ''
+        return this.http.get(this.baseUrl + '/getall?' + keyword + type + status + priority
+        + permision + createDateFrom + createDateTo + deadlineDateFrom + deadlineDateTo + departmentId + userId);
+    }else{
+      return this.http.get(this.baseUrl + '/getall' );
+    }
 
   }
   createProject(payload:any):Observable<any>{
