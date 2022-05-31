f5dd28b425594d9e1077b2a0f8f53c7f7be8c975
Binh Hoang <hbinhit221@gmail.com>
2022-05-31 18:21:25 +0700

    - Sua calendar

--- client/src/app/routes/tasks/partials/tasks-calendar/tasks-calendar.component.ts
+++ client/src/app/routes/tasks/partials/tasks-calendar/tasks-calendar.component.ts
@@ -1,5 +1,12 @@
 import { DatePipe } from '@angular/common';
-import { Component, ElementRef, forwardRef, Injector, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  forwardRef,
+  Injector,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Calendar } from '@fullcalendar/core';
 import interactionPlugin from '@fullcalendar/daygrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -13,70 +20,70 @@ import { TasksComponent } from '../../tasks.component';
 @Component({
   selector: 'app-tasks-calendar',
   templateUrl: './tasks-calendar.component.html',
-  styleUrls: ['./tasks-calendar.component.css']
+  styleUrls: ['./tasks-calendar.component.css'],
 })
 export class TasksCalendarComponent extends TasksComponent implements OnInit {
-  @ViewChild('fullcalendar') fullcalendar: ElementRef
+  @ViewChild('fullcalendar') fullcalendar: ElementRef;
   calendarOptions: CalendarOptions = {
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
     },
     initialView: 'dayGridMonth',
     weekends: true,
     editable: true,
     selectable: true,
-    contentHeight:'100%'
   };
   ngOnInit(): void {
-    this.route.params.subscribe(params=>{
+    this.route.params.subscribe((params) => {
       this.projectId = params['projectId'];
-   })
-  const user = JSON.parse(localStorage.getItem('user'))
-  this.user = JSON.parse(localStorage.getItem('user'));
-  if(user.permissionCode === Permission.ProjectManager || user.permissionCode === Permission.Leader){
-    this.right = true
-  }
-  this.userId = this.user.id;
-  this.fetchTaskData();
-  this.fetchUserData();
-  this.fetchProjectData();
-}
-
-fetchTaskData() {
-  this.sub = this.taskService
-    .getAllTask(this.projectId, this.userId, this.getAllTask)
-    .pipe(catchError((err) => of(err)))
-    .subscribe((response) => {
-      this.tasks = response;
-      let array = []
-      this.tasks.forEach(task=>{
-        const detail = {
-          title: task.taskName,
-          start: task.createDate,
-          end: task.deadlineDate,
-        }
-        array.push(detail);
-      })
-      this.calendarOptions = {
-        initialView: 'dayGridMonth',
-        eventClick: this.onDateClick.bind(this),
-        events: array,
-        editable: true,
-      droppable: true,
-      themeSystem: 'bootstrap',
-      height:'80%',
-      eventColor: '#00b4d8'
-
-      };
-
     });
-}
+    const user = JSON.parse(localStorage.getItem('user'));
+    this.user = JSON.parse(localStorage.getItem('user'));
+    if (
+      user.permissionCode === Permission.ProjectManager ||
+      user.permissionCode === Permission.Leader
+    ) {
+      this.right = true;
+    }
+    this.userId = this.user.id;
+    this.fetchTaskData();
+    this.fetchUserData();
+    this.fetchProjectData();
+  }
 
-onDateClick(res: any) {
-  alert('Clicked on date : ' + res);
-}
+  fetchTaskData() {
+    this.sub = this.taskService
+      .getAllTask(this.projectId, this.userId, this.getAllTask)
+      .pipe(catchError((err) => of(err)))
+      .subscribe((response) => {
+        this.tasks = response;
+        let array = [];
+        this.tasks.forEach((task) => {
+          const detail = {
+            title: task.taskName,
+            start: task.createDate,
+            end: task.deadlineDate,
+          };
+          array.push(detail);
+        });
+        this.calendarOptions = {
+          initialView: 'dayGridMonth',
+          eventClick: this.onDateClick.bind(this),
+          events: array,
+          editable: true,
+          droppable: true,
+          themeSystem: 'bootstrap',
+          eventColor: '#00b4d8',
+          height: 630,
+        };
+      });
+  }
+
+  onDateClick(res: any) {
+    alert('Clicked on date : ' + res);
+  }
   pipeDate(date: any) {
     const datePipe = new DatePipe('en-US');
     return datePipe.transform(date, 'dd-MM-yyyy');
@@ -93,11 +100,9 @@ onDateClick(res: any) {
   dateClick(model) {
     console.log(model);
   }
-  handleClickDate(){}
-  handleEventClick(){
+  handleClickDate() {}
+  handleEventClick() {
     console.log(123);
-
   }
-  handleDropEvent(){}
+  handleDropEvent() {}
 }
-
