5dae4d27d85616b95282367dea69c1e92a43aafb
binhpnt <bykmaimai@gmail.com>
2022-06-02 14:41:18 +0700

    Design-Task

---
+++ API/Entity/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entity
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid SenderId { get; set; }
+        public string SenderUserName { get; set; }
+        public AppUser Sender { get; set; }
+        public Guid RecipientId { get; set; }
+        public string RecipientUserName { get; set; }
+        public AppUser Recipient { get; set; }
+        public string Content { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime MessageSent { get; set; }
+        public bool SenderDeleted { get; set; }
+        public bool RecipientDeleted { get; set; }
+    }
+}
