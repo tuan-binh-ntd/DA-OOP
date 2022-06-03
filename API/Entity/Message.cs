accd4f2896714bc665eb76a390f15dbeca3cd359
binhpnt <bykmaimai@gmail.com>
2022-06-03 09:35:14 +0700

    Add-Entity-Message

--- API/Entity/Message.cs
+++ API/Entity/Message.cs
@@ -1,22 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Entity
 {
     public class Message
     {
+        [Required, Key]
         public Guid Id { get; set; }
+        [Required, ForeignKey("Task")]
+        public Guid TaskId { get; set; }
+        [Required, ForeignKey("AppUser")]
         public Guid SenderId { get; set; }
+        [Required, StringLength(40)]
         public string SenderUserName { get; set; }
         public AppUser Sender { get; set; }
+        [Required, ForeignKey("AppUser")]
         public Guid RecipientId { get; set; }
+        [Required, StringLength(40)]
         public string RecipientUserName { get; set; }
         public AppUser Recipient { get; set; }
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
-        public DateTime MessageSent { get; set; }
+        public DateTime MessageSent { get; set; } = DateTime.Now;
         public bool SenderDeleted { get; set; }
         public bool RecipientDeleted { get; set; }
     }
---
+++ API/Migrations/20220602095753_AddMessagesToDB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddMessagesToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TaskId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SenderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SenderUserName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    RecipientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RecipientUserName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateRead = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    MessageSent = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SenderDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    RecipientDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_AppUser_RecipientId",
+                        column: x => x.RecipientId,
+                        principalTable: "AppUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_AppUser_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "AppUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
