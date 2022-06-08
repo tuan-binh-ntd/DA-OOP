4f294c1d5cfc110e4e2c4cc10ddcf7d8167ca93e
ntdhunter <bykmaimai@gmail.com>
2022-06-08 16:20:14 +0700

    Last_Fix

--- API/DTO/TaskDto/ModifyTaskDto.cs
+++ API/DTO/TaskDto/ModifyTaskDto.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace API.DTO
 {
-    public class TaskDto
+    public class ModifyTaskDto
     {
         public string TaskName { get; set; }
         public string CreateUserName { get; set; }
