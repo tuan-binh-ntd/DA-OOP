ae284b2ab58665916d7116f5b0d6d8a5cb1377df
binhpnt <bykmaimai@gmail.com>
2022-06-13 09:32:16 +0700

    Add_Photo_Table

---
+++ API/Entity/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entity
+{
+    public class Photo
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+    }
+}
