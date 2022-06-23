b3e30dce23d048f4bdccb187478613c5f8b43d03
binhpnt <bykmaimai@gmail.com>
2022-06-23 15:32:34 +0700

    Add_API_SetMainPhoto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -339,5 +339,48 @@ namespace API.Controllers
             await _dataContext.SaveChangesAsync();
             return CreatedAtRoute("GetUser", new { username = user.FirstName + ' ' + user.LastName }, photo);
         }
+
+        [HttpPut("setmainphoto/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(Guid photoId, Guid id)
+        {
+            var user = await _dataContext.AppUser.FindAsync(id);
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo.IsMain) return BadRequest("This is already your main photo");
+
+            var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+            if (currentMain != null) currentMain.IsMain = false;
+            photo.IsMain = true;
+
+            if (await _dataContext.SaveChangesAsync() > 0) return NoContent();
+
+            return BadRequest("Failed to set main photo");
+        }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(Guid photoId, Guid id)
+        {
+            var user = await _dataContext.AppUser.FindAsync(id);
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("You cannot delete your main photo");
+
+            if (photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if (await _dataContext.SaveChangesAsync() > 0) return Ok();
+
+            return BadRequest("Failed to delete your photo");
+        }
+
     }
 }
