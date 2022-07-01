af237424bdd64882f309f4828cfbc821c72beb11
Binh Hoang <hbinhit221@gmail.com>
2022-07-01 22:52:00 +0700

    - Sua layout dashboard

--- client/src/app/routes/home-v2/home-v2.component.ts
+++ client/src/app/routes/home-v2/home-v2.component.ts
@@ -1,8 +1,11 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { catchError, of } from 'rxjs';
+import { StatusCode } from 'src/app/helpers/StatusCodeEnum';
 import { GetAllProject } from 'src/app/models/getallproject';
+import { GetAllTask } from 'src/app/models/getalltask';
 import { ProjectService } from 'src/app/services/project.service';
+import { TaskService } from 'src/app/services/task.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -16,14 +19,36 @@ export class HomeV2Component implements OnInit {
   isLoading: boolean = false;
   projects:any[] = [];
   totalProjects: number = 0;
+  reOpenProject: number = 0;
+  openProject: number = 0;
+  inProgressProject: number = 0;
+  resolvedProject: number = 0;
+  getAllTask: GetAllTask = new GetAllTask();
   data :any[] = [];
-  constructor(private userSerivice: UserService, private projectService: ProjectService) { }
+  tasks: any[] = [];
+  pieData: any[] = []
+  constructor(private userSerivice: UserService, private projectService: ProjectService,
+    private taskService: TaskService,
+    ) { }
 
   async ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('user'));
     this.getAllProject.departmentId = this.user.departmentId
     await this.fetchProjectData();
+     this.fetchTaskData();
     // this.divideProjectData();
+  }
+
+  fetchTaskData() {
+    this.isLoading = true;
+    this.taskService
+      .getAllTask(this.getAllTask)
+      .pipe(catchError((err) => of(err)))
+      .subscribe((response) => {
+        this.tasks = response;
+          this.tasks = this.tasks.filter(t => t.createUserId !== t.appUserId);
+        this.isLoading = false;
+      });
   }
 
  async fetchProjectData(){
@@ -33,23 +58,54 @@ export class HomeV2Component implements OnInit {
       .getAllProject(this.getAllProject)
       .pipe(catchError((err) => of(err)))
       .toPromise().then((response) => {
+        debugger
         this.projects = response;
         this.totalProjects = response.length;
-        const obj = {
-          name:'Tasks',
-          series:[]
-        }
-        this.projects.forEach((project)=>{
-          obj.series.push({
-            value: project.taskCount,
-            name: this.pipeDate(project.deadlineDate)
-          })
-        })
-        this.data.push(obj);
-        this.data = [...this.data];
+        this.pushLineChartData();
+        this.pushPieChartData();
         this.isLoading = false;
       });
   }
+
+    pushPieChartData(){
+      this.reOpenProject = this.projects.filter(project=> project.statusCode === StatusCode.Reopened).length;
+      this.openProject = this.projects.filter(project=> project.statusCode === StatusCode.Open).length;
+      this.inProgressProject = this.projects.filter(project=> project.statusCode === StatusCode.InProgress).length;
+      this.resolvedProject = this.projects.filter(project=> project.statusCode === StatusCode.Resolved).length;
+      this.pieData = [
+        {
+          name: "Germany",
+          value: 40632,
+          extra: {
+            code: "de"
+          }
+        },
+        {
+          "name": "United States",
+          "value": 50000,
+          "extra": {
+            "code": "us"
+          }
+        }
+        
+      ]
+      }
+
+    pushLineChartData(){
+      const line = {
+        name:'Project',
+        series:[]
+      }
+      this.projects.forEach((project)=>{
+        line.series.push({
+          value: project.taskCount,
+          name: project.projectName
+        })
+      })
+      this.data.push(line);
+      this.data = [...this.data];
+
+    }
 
   pipeDate(date: any) {
     const datePipe = new DatePipe('en-US');
